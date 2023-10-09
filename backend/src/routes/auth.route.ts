085614d18d45a6bb15c35f63b7e3e756b7b5358b
d-rigel <alozie4God@gmail.com>
2023-10-09 10:50:36 -0700

    added logout endpoint swagger documentation

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -158,6 +158,35 @@ router.post('/verify-email', validate(authValidation.verifyEmail), authControlle
  *         $ref: '#/components/responses/NotFound'
  */
 
+/**
+ * @swagger
+ * /auth/forgot-password:
+ *   post:
+ *     summary: Forgot password
+ *     description: An email will be sent to reset password.
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *             example:
+ *               email: fake@example.com
+ *     responses:
+ *       "204":
+ *         description: No content
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
+
+
 
 
 
