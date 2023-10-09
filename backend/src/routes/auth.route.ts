cfb8e85f4fd8ed2a9bdcce29fa2327f78f0d01fa
d-rigel <alozie4God@gmail.com>
2023-10-09 10:39:44 -0700

    added refresh token endpoint

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -132,6 +132,33 @@ router.post('/verify-email', validate(authValidation.verifyEmail), authControlle
  *               message: Invalid email or password
  */
 
+/**
+ * @swagger
+ * /auth/logout:
+ *   post:
+ *     summary: Logout
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refreshToken
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *             example:
+ *               refreshToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWJhYzUzNDk1NGI1NDEzOTgwNmMxMTIiLCJpYXQiOjE1ODkyOTg0ODQsImV4cCI6MTU4OTMwMDI4NH0.m1U63blB0MLej_WfB7yC2FTMnCziif9X8yzwDEfJXAg
+ *     responses:
+ *       "204":
+ *         description: No content
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
+
+
 
 
 export default router;
